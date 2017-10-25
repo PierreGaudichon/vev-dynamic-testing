96368edcb8583494647ab1a601b5ddf81d00906c
timMerlet <timothee.merlet.thomazeau@gmail.com>
2017-10-25 09:52:01 +0200

    add_run_junit_method

--- src/main/java/istic/fr/vev_dynamic_testing/App.java
+++ src/main/java/istic/fr/vev_dynamic_testing/App.java
@@ -1,5 +1,16 @@
 package istic.fr.vev_dynamic_testing;
 
+import javassist.CannotCompileException;
+import javassist.ClassPool;
+import javassist.Loader;
+import javassist.NotFoundException;
+import javassist.Translator;
+
+import org.junit.internal.TextListener;
+import org.junit.runner.JUnitCore;
+
+import istic.fr.prog_test.*;
+
 /**
  * Hello world!
  *
@@ -8,6 +19,16 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	// first step : modify the Junit code
+    	
+    	// second step : run Junit
+    	runningStation();
+    	 
     }
+    
+    public static void runningStation() {
+  	    JUnitCore runner = new JUnitCore();
+  	    runner.addListener(new TextListener(System.out));
+  	    runner.run(PointTest.class);
+  	}
 }
