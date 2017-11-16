1de939d3d22b002bf15fc431404a6044d3ed0214
pierre <pierre@fedora.pierre>
2017-11-16 10:40:24 +0100

    Make test project pass its tests from main project.

--- src/main/java/istic/fr/vev_dynamic_testing/App.java
+++ src/main/java/istic/fr/vev_dynamic_testing/App.java
@@ -4,12 +4,22 @@ package istic.fr.vev_dynamic_testing;
 import javassist.*;
 import org.junit.internal.TextListener;
 import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
 
 /**
  * Hello world!
  *
  */
 public class App {
+
+    public final static String TEST_PROJECT = "src/test/resources/Test1";
+    public final static String TEST_CLASS = "istic.fr.prog_test.PointTest";
 
     private static class MyTranslator implements Translator {
         public void start(ClassPool pool) throws NotFoundException, CannotCompileException {
@@ -30,22 +40,27 @@ public class App {
         PointTest
    }*/
    
-    public static void main( String[] args ) throws NotFoundException
-    {
+    public static void main( String[] args ) throws NotFoundException, MalformedURLException, ClassNotFoundException {
     	// first step : modify the Junit code
-    	ClassPool pool = ClassPool.getDefault();
-    	pool.appendClassPath("src/test/resources/Test1/target/test-classes/istic/fr/prog_test");
-    	CtClass cc = pool.get("istic.fr.prog_test.PointTest");
-
-
+    	//ClassPool pool = ClassPool.getDefault();
+    	//pool.appendClassPath(TEST_PROJECT + "/target/test-classes/istic/fr/prog_test");
+    	//CtClass cc = pool.get(TEST_CLASS);
     	// second step : run Junit
-    	runningStation(cc);
-    	 
+    	//runningStation(cc);
+
+        // Réponse de Nicolas
+        ClassLoader cl = new URLClassLoader(new URL[]{
+                new File(TEST_PROJECT + "/target/test-classes").toURL(),
+                new File(TEST_PROJECT + "/target/classes").toURL()
+        });
+
+        //On lance JUnit sur la class de test
+        runningStation(cl.loadClass(TEST_CLASS));
     }
     
-    public static void runningStation(CtClass cc) {
+    public static void runningStation(Class cc) {
   	    JUnitCore runner = new JUnitCore();
   	    runner.addListener(new TextListener(System.out));
-  	    runner.run(cc.getClass());
+  	    runner.run(cc);
   	}
 }
