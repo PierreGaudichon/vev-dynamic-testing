f14fb3301bc0cca59da27184e05c54ba7a79b459
pierre <pierre@fedora.pierre>
2017-11-17 15:37:33 +0100

    Create build scripts.

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+md5sum src/test/resources/Test1/target/classes/istic/fr/prog_test/Point.class
---
+++ build_test_project.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd src/test/resources/Test1/
+mvn clean install
+md5sum target/classes/istic/fr/prog_test/Point.class
