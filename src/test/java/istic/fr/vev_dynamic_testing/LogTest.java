32e8bc6ac65a0ce0b8b8a2b5f06d73fa361c6cb9
timMerlet <timothee.merlet.thomazeau@gmail.com>
2017-11-27 08:58:19 +0100

    add_first_test

---
+++ src/test/java/istic/fr/vev_dynamic_testing/LogTest.java
@@ -0,0 +1,36 @@
+package istic.fr.vev_dynamic_testing;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class LogTest extends TestCase {
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public LogTest( String testName ) {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite() {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Test Constructor
+     */
+    public void testConstructorLog() {
+        
+    	Log l = new Log ("a","b");
+    	
+    	assertTrue( l.getType().isEquals("a") );
+    }
+}
