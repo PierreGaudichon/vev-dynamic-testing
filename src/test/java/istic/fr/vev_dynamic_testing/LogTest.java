4e09927e54720d0137179f9780467400392b0692
pierre <pierre@fedora.pierre>
2017-12-16 19:31:45 +0100

    Add tests for Log.

--- src/test/java/istic/fr/vev_dynamic_testing/LogTest.java
+++ src/test/java/istic/fr/vev_dynamic_testing/LogTest.java
@@ -28,11 +28,47 @@ public class LogTest extends TestCase {
      * Test Constructor
      */
     public void testConstructorLog() {
-        
     	Log l = new Log (Log.IO.BEGIN, Log.TYPE.METHOD, "b");
-
     	assertTrue(l.getIo().equals(Log.IO.BEGIN));
     	assertTrue(l.getType().equals(Log.TYPE.METHOD));
     	assertTrue(l.getMessage().equals("b"));
+    }
+
+    public void testToStatement() {
+        Log l = new Log(Log.IO.BEGIN, Log.TYPE.BLOCK, "name");
+        String expected = "Logs.getInstance().addLogs(\"BEGIN\", \"BLOCK\", \"name\");";
+        assertEquals(expected, l.toStatement());
+    }
+
+    public void testIsBeginBlock() {
+        Log l1 = new Log(Log.IO.BEGIN, Log.TYPE.BLOCK, "name");
+        assertTrue(l1.isBeginBlock());
+        Log l2 = new Log(Log.IO.END, Log.TYPE.BLOCK, "name");
+        assertFalse(l2.isBeginBlock());
+    }
+
+    public void testIsBeginMethod() {
+        Log l1 = new Log(Log.IO.BEGIN, Log.TYPE.METHOD, "name");
+        assertTrue(l1.isBeginMethod());
+        Log l2 = new Log(Log.IO.END, Log.TYPE.METHOD, "name");
+        assertFalse(l2.isBeginMethod());
+        Log l3 = new Log(Log.IO.BEGIN, Log.TYPE.BLOCK, "name");
+        assertFalse(l3.isBeginMethod());
+    }
+
+    public void testIsDeclaringBlock() {
+        Log l1 = new Log(Log.IO.DECLARING, Log.TYPE.BLOCK, "name");
+        assertTrue(l1.isDeclaringBlock());
+        Log l2 = new Log(Log.IO.END, Log.TYPE.BLOCK, "name");
+        assertFalse(l2.isDeclaringBlock());
+    }
+
+    public void testIsCallingMethod() {
+        Log l1 = new Log(Log.IO.CALLING, Log.TYPE.METHOD, "name");
+        assertTrue(l1.isCallingMethod());
+        Log l2 = new Log(Log.IO.CALLING, Log.TYPE.BLOCK, "name");
+        assertFalse(l2.isCallingMethod());
+        Log l3 = new Log(Log.IO.END, Log.TYPE.METHOD, "name");
+        assertFalse(l3.isCallingMethod());
     }
 }
