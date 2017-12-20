74b4cbbfccde561644baedd5482ab8c0da59be74
pierre <pierre@fedora.pierre>
2017-12-20 23:58:48 +0100

    Generalize project. Bugged for commons-cli.

--- src/main/java/istic/fr/vev_dynamic_testing/App.java
+++ src/main/java/istic/fr/vev_dynamic_testing/App.java
@@ -6,6 +6,8 @@ import javassist.CannotCompileException;
 import javassist.ClassPool;
 import javassist.CtClass;
 import javassist.NotFoundException;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
 import org.junit.internal.TextListener;
 import org.junit.runner.JUnitCore;
 
@@ -14,6 +16,8 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Hello world!
@@ -21,18 +25,34 @@ import java.net.URLClassLoader;
  */
 public class App {
 
+    // Test1 - Point
     public final static String TEST_PROJECT = "examples/Test1";
-    public final static String TEST_CLASS = "istic.fr.prog_test.PointTestNot";
-    public final static String MAIN_CLASS = "istic.fr.prog_test.Point";
+    public final static String PACKAGE_NAME = "istic.fr.prog_test";
+    public final static String BUILD_COMMAND = "./build_test_project.sh";
     
+    /*// commons-cli
+    public final static String TEST_PROJECT = "examples/commons-cli";
+    public final static String PACKAGE_NAME = "org.apache.commons.cli";
+    public final static String BUILD_COMMAND = "./clone_and_build.sh";
+    */
+
     // ctClass qui représente le logger
     // private static CtClass logs = null;
 
-    public static void buildProject() throws IOException, InterruptedException {
-        Process process = Runtime.getRuntime().exec("./build_test_project.sh");
-        process.waitFor();
-        // Runtime.getRuntime().exec("./clone_and_build.sh");
+    public static List<String> getJavaClassNames(String folderPath) {
+        File folder = new File(folderPath);
+        String[] extentions = new String[] { "java" };
+        return FileUtils.listFiles(folder, extentions, true).stream()
+                .map(file -> FilenameUtils.removeExtension(file.getName()))
+                .collect(Collectors.toList());
     }
+
+
+    public static void buildProject() throws IOException, InterruptedException {
+        Process process = Runtime.getRuntime().exec(BUILD_COMMAND);
+        process.waitFor();
+    }
+
 
     public static void modifyMain() throws NotFoundException, IOException, CannotCompileException {
         ClassPool pool = ClassPool.getDefault();
@@ -46,25 +66,28 @@ public class App {
         pool.importPackage("istic.fr.vev_dynamic_testing");
         pool.importPackage("istic.fr.vev_dynamic_testing.Log");
 
-        System.out.println(pool.find("Logs"));
-
-        CtClass cc = pool.get(MAIN_CLASS);
-        // ClassLogger classLogger = new ClassLogger(cc, logs);
-        ClassLogger classLogger = new ClassLogger(cc, null);
-        classLogger.makeLogs();
-
-        cc.writeFile(TEST_PROJECT + "/target/classes");
+        for(String name : getJavaClassNames(TEST_PROJECT + "/src/main")) {
+            CtClass cc = pool.get(PACKAGE_NAME + "." + name);
+            new ClassLogger(cc, null).makeLogs();
+            cc.writeFile(TEST_PROJECT + "/target/classes");
+        }
     }
+
 
     public static void runTest() throws MalformedURLException, ClassNotFoundException {
         ClassLoader classLoader = new URLClassLoader(new URL[]{
                 new File(TEST_PROJECT + "/target/test-classes").toURL(),
                 new File(TEST_PROJECT + "/target/classes").toURL()
-        });
-        Class cc = classLoader.loadClass(TEST_CLASS);
+        }, ClassLoader.getSystemClassLoader());
+        /*Class cc = classLoader.loadClass(TEST_CLASS);
         JUnitCore runner = new JUnitCore();
         runner.addListener(new TextListener(System.out));
-        runner.run(cc);
+        runner.run(cc);*/
+        JUnitCore runner = new JUnitCore();
+        runner.addListener(new TextListener(System.out));
+        for(String name : getJavaClassNames(TEST_PROJECT + "/src/test")) {
+            runner.run(classLoader.loadClass(PACKAGE_NAME + "." + name));
+        }
     }
 
     /*public static void runTest() throws IOException, InterruptedException {
@@ -83,6 +106,7 @@ public class App {
         Report.print(r.methodCallSequence());
     }
 
+
     public static void main( String[] args ) throws NotFoundException, IOException, ClassNotFoundException, CannotCompileException, InterruptedException {
         // Reset logs.
     	Logs.getInstance().removeLogs();
@@ -95,5 +119,7 @@ public class App {
         // on récupère les logs de l'exécution et on les affiche
         printReports();
     }
+
+
 
 }
